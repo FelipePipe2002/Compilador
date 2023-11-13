8c497edc1e950da5f9ff68da4c92b02cf619fe24
SanchezV8 <fernandoagustinsanchez@hotmail.com>
2023-11-13 11:56:38 -0300

    Se agregan scripts para compilar y ejecutar

---
+++ ejecutar.bat
@@ -0,0 +1,25 @@
+@echo off
+cd .\Sintactico\
+.\yacc.exe -J .\gramatica.y
+cd ..
+set "line=package Sintactico;"
+set "file=.\Sintactico\ParserVal.java"
+
+(
+  echo %line%
+  type %file%
+) > %file%.tmp
+
+move /y %file%.tmp %file%
+
+echo Compilando...
+javac .\Lexico\*.java
+javac .\Sintactico\*.java
+
+if errorlevel 1 (
+    echo Error de compilacion. No se pudo compilar Parser.java.
+) else (
+    echo Compilacion exitosa. Ejecutando el programa...
+    java .\Sintactico\Parser.java ./Code.txt
+)
+
